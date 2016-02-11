ef833191db44a9f5715e328f299ac0d33dd61c1b
Kyle Linneman <clodewerks@gmail.com>
2016-02-10 21:34:50 -0800

    initial commit

---
+++ live.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Waffle Taco</title>
+		<link rel="shortcut icon" href="/images/weblogo.ico" />
+		<link rel='stylesheet' href='/style/style.css' type='text/css' />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+			<h1>Waffle Taco</h1>
+			</div>
+			<div id="nav">
+				<a href="index.html"> About </a>
+				<a href="listen.html">Listen</a>
+				<a href="live.php">Live</a>
+				<a href="mailto:waffletaco@clodewerks.com">Contact</a>
+				<a href="http://waffletaco.bandcamp.com/">Shop</a>
+				<a id="fb" href="https://www.facebook.com/waffletacopdx"></a>
+			</div>
+			<div id="content">
+				<h2>Live</h2>
+					<a href="http://www.songkick.com/artists/5993109" class="songkick-widget" data-theme="light" data-track-button="on" data-detect-style="true" data-background-color="transparent">Waffle Taco tour dates</a>
+					<script src="//widget.songkick.com/widget.js"></script>
+<p> For booking inquiries, send an email to <a href="mailto:waffletaco@clodewerks.com">Waffle Taco</a>.</p>
+</div>
+		</div>
+		<div id="footer">&#169; 2013 Waffle Taco</div>
+	</body>
+</html>
